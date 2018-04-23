892463a123bb6d86026afec512b6ea2d73e8bd2a
Michael Giordano <magiordano77@gmail.com>
2018-04-23 14:21:08 -0600

    test

---
+++ MagTestApp.js
@@ -0,0 +1,22 @@
+var express= require('express');
+
+var app =  express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static('assets'))
+app.use(express.static('views'));
+
+app.get('/', function(req,res){
+	res.sendFile(_dirname + "/" + "index.html");
+});
+
+
+
+var server = app.listen(8081, function(){
+	var host= server.address().address
+	var port = server.address().port
+
+	console.log("example app listening at http://%s:%s", host, port)
+})
