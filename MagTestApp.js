3433c43622ff09356832e400c0be1cca8c86dc54
michael Giordano <magiordano77.com>
2018-05-09 18:32:15 +0000

    Kameron is the best developer NA

--- MagTestApp.js
+++ MagTestApp.js
@@ -5,7 +5,7 @@ var app =  express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(express.static('assets'))
+app.use(express.static('assets'));
 app.use(express.static('views'));
 
 app.get('/', function(req,res){
